748d15e41673dec227d11bf36589b097badc994d
Serhiy <sergy200478@gmail.com>
2024-06-21 19:03:59 +0300

    loops edded

--- js/script.js
+++ js/script.js
@@ -10,12 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const userFilmName1 = prompt("One of the films you previosly watched?"),
-      userFilmRating1 = +prompt("Please, rate it"),
-      userFilmName2 = prompt("One of the films you previosly watched?"),
-      userFilmRating2 = +prompt("Please, rate it");
+let i = 0;
 
-personalMovieDB.movies[userFilmName1] = userFilmRating1;
-personalMovieDB.movies[userFilmName2] = userFilmRating2;
+do {
+    const userFilmName = prompt("One of the films you previosly watched?"),
+          userFilmRating = +prompt("Please, rate it");
+    if (userFilmName === null || userFilmName.length > 50 || userFilmName === "") {
+        i--;
+    } else {
+        personalMovieDB.movies[userFilmName] = userFilmRating;
+    }
+    i++;
+} while (i < 2)
+
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+    alert('Преглянуто достатньо мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Ви класичний глядач");
+} else if (personalMovieDB.count >= 30) {
+    alert("Ви кіноман");
+} else {
+    alert("Відбулася помилка");
+}
 
 console.log(personalMovieDB);
