997f22c18b507824955bf4fa416c4d19433238d8
Serhiy <sergy200478@gmail.com>
2024-06-27 02:23:11 +0300

    functions replaced with methods, toggleVisibleMyDB added, writeMyGeners updated

--- js/script.js
+++ js/script.js
@@ -2,73 +2,86 @@
 
 let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("how many films have you watched?");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("how many films have you watched?");
-    }
-}
-
-start();
-
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [], 
-    privat: false
+    privat: false,
+    start: function() {
+        numberOfFilms = +prompt("how many films have you watched?");
+
+        while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt("how many films have you watched?");
+        }
+        personalMovieDB.count = numberOfFilms;
+    },
+    showMyDB: function(privat) {
+        if (!privat) {
+            console.log(personalMovieDB);
+        }
+    },
+    rememberMyFilms: function() {
+        let i = 0;
+
+        do {
+            const userFilmName = prompt("One of the films you previosly watched?"),
+                userFilmRating = +prompt("Please, rate it");
+            if (userFilmName === null || userFilmName.length > 50 || userFilmName === "") {
+                i--;
+            } else {
+                personalMovieDB.movies[userFilmName] = userFilmRating;
+            }
+            i++;
+        } while (i < 2)
+    },
+    writeMyGenres: function() {
+        for (let i = 0; i < 1; i++) {
+            const userGenres = prompt("Запишіть ваші улюбленні жанри через кому");
+            if (userGenres === "" || userGenres === null) {
+                i--;
+            } else {
+                personalMovieDB.genres = userGenres.split(", ");
+                personalMovieDB.genres.sort();
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Улюблений жанр #${i+1} - це ${item}`);
+        })        
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+            alert('Преглянуто достатньо мало фільмів');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert("Ви класичний глядач");
+        } else if (personalMovieDB.count >= 30) {
+            alert("Ви кіноман");
+        } else {
+            alert("Відбулася помилка");
+        }        
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }
 };
 
-function showMyDB(privat) {
-    if (privat === false) {
-        console.log(personalMovieDB);
-    }
-}
 
-showMyDB(personalMovieDB.privat);
+personalMovieDB.start();
+
+personalMovieDB.toggleVisibleMyDB();
+
+personalMovieDB.showMyDB(personalMovieDB.privat);
+
+personalMovieDB.rememberMyFilms();
+
+personalMovieDB.writeMyGenres();
+
+personalMovieDB.detectPersonalLevel();
 
 
-
-function rememberMyFilms() {
-    let i = 0;
-
-    do {
-        const userFilmName = prompt("One of the films you previosly watched?"),
-            userFilmRating = +prompt("Please, rate it");
-        if (userFilmName === null || userFilmName.length > 50 || userFilmName === "") {
-            i--;
-        } else {
-            personalMovieDB.movies[userFilmName] = userFilmRating;
-        }
-        i++;
-    } while (i < 2)
-}
-
-rememberMyFilms();
-
-function writeMyGenres() {
-    for (let i = 0; i < 3; i++) {
-        const userGenre = prompt(`Ваш олюбленний жанр під номером ${i+1}`);
-        personalMovieDB.genres[i] = userGenre;
-    }
-}
-
-writeMyGenres();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
-        alert('Преглянуто достатньо мало фільмів');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert("Ви класичний глядач");
-    } else if (personalMovieDB.count >= 30) {
-        alert("Ви кіноман");
-    } else {
-        alert("Відбулася помилка");
-    }    
-}
-
-detectPersonalLevel();
 
 
